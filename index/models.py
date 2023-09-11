ed8acc6a67d17d435baad61c8bd0cb0818637acf
rylabdumamag <91299883+rylabdumamag@users.noreply.github.com>
2023-09-11 15:35:04 +0800

    so far oks

--- index/models.py
+++ index/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
-class BingoCard(models.Model):
-    b_column = models.CharField(max_length=100)  # Store B column numbers as a comma-separated string
-    i_column = models.CharField(max_length=100)  # Store I column numbers as a comma-separated string
-    n_column = models.CharField(max_length=100)  # Store N column numbers as a comma-separated string
-    g_column = models.CharField(max_length=100)  # Store G column numbers as a comma-separated string
-    o_column = models.CharField(max_length=100)  # Store O column numbers as a comma-separated string
+
 # Create your models here.
 
+
+
+class BingoCard(models.Model):
+    b_column = models.CharField(max_length=100)  
+    i_column = models.CharField(max_length=100) 
+    n_column = models.CharField(max_length=100) 
+    g_column = models.CharField(max_length=100) 
+    o_column = models.CharField(max_length=100) 
 class RaffleEntry(models.Model):
     ticket_number = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
--- static/bingo.js
+++ static/bingo.js
@@ -1,26 +1,50 @@
-        // JavaScript code to handle random number scrolling
-        const numberContainer = document.getElementById('number-container');
-        const startButton = document.getElementById('start-button');
-        const stopButton = document.getElementById('stop-button');
-        let interval;
-    
-        function startScrolling() {
-            clearInterval(interval); // Clear any existing interval
-            let startTime = Date.now();
-            
-            interval = setInterval(function () {
-                const elapsedTime = Date.now() - startTime;
-                if (elapsedTime >= 5000) {
-                    clearInterval(interval); // Stop scrolling after 5 seconds
-                    const randomNumber = Math.floor(Math.random() * 75) + 1; // Random numbers between 1 and 75
-                    numberContainer.textContent = randomNumber;
-                } else {
-                    const randomNumber = Math.floor(Math.random() * 75) + 1; // Random numbers between 1 and 75
-                    numberContainer.textContent = randomNumber;
-                }
-                
-            }, 100);
-        }
-    
-        startButton.addEventListener('click', startScrolling);
+// JavaScript code to handle random number scrolling
+const numberContainer = document.getElementById('number-container');
+const startButton = document.getElementById('start-button');
+const bingoTable = document.getElementById('bingo-table');
+let interval;
 
+function getRandomNumber() {
+    return Math.floor(Math.random() * 75) + 1; // Random numbers between 1 and 75
+}
+
+function startScrolling() {
+    clearInterval(interval); // Clear any existing interval
+    let startTime = Date.now();
+
+    interval = setInterval(function () {
+        const elapsedTime = Date.now() - startTime;
+        if (elapsedTime >= 5000) {
+            clearInterval(interval); // Stop scrolling after 5 seconds
+            const randomNumber = getRandomNumber();
+            numberContainer.textContent = randomNumber;
+
+            // Display the selected number in the corresponding column
+            let columnId = '';
+            if (randomNumber >= 1 && randomNumber <= 15) {
+                columnId = 'b-column';
+            } else if (randomNumber >= 16 && randomNumber <= 30) {
+                columnId = 'i-column';
+            } else if (randomNumber >= 31 && randomNumber <= 45) {
+                columnId = 'n-column';
+            } else if (randomNumber >= 46 && randomNumber <= 60) {
+                columnId = 'g-column';
+            } else {
+                columnId = 'o-column';
+            }
+
+            const column = document.getElementById(columnId);
+            if (column.textContent === '') {
+                column.textContent = randomNumber;
+            }
+
+            // Remove the selected number from the scroll
+            numberContainer.textContent = '';
+        } else {
+            const randomNumber = getRandomNumber();
+            numberContainer.textContent = randomNumber;
+        }
+    }, 100);
+}
+
+startButton.addEventListener('click', startScrolling);
