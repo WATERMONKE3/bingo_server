f5e2fd888222ac97c6c959d2ba7ce705031f03e1
jazferlex <79888058+jazferlex@users.noreply.github.com>
2023-09-11 14:07:44 +0800

    asdfsdf

--- index/views.py
+++ index/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'lp.html')
+
+def raffle(request):
+    return render(request, 'raffle.html')
