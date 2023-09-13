90121fde98764baea04d15b9d95b7390ea4d5a8f
rylabdumamag <91299883+rylabdumamag@users.noreply.github.com>
2023-09-13 13:43:14 +0800

    rtwerewr

--- static/bingo.js
+++ static/bingo.js
@@ -2,7 +2,7 @@ function startScroll() {
     // Disable the Start button to prevent multiple selections
     document.getElementById('start-button').disabled = true;
 
-    // Function to generate and display a random number every 100 milliseconds
+    // Function to generate and display a random number every 1000 milliseconds (1 second)
     function scrollNumber() {
         const randomNumber = Math.floor(Math.random() * 75) + 1;
         document.getElementById('number-container').textContent = randomNumber;
@@ -12,14 +12,14 @@ function startScroll() {
         if (randomNumber !== 0) {
             document.getElementById('bingo-form').submit();
         } else {
-            // Continue scrolling numbers
-            setTimeout(scrollNumber, 100);
+            // Continue scrolling numbers after a 1-second interval
+            setTimeout(scrollNumber, 5000);
         }
     }
 
-    // Start scrolling numbers for 3 seconds (3000 milliseconds)
+    // Start scrolling numbers for 5 seconds (5000 milliseconds)
     setTimeout(function () {
-        // Stop scrolling after 3 seconds
+        // Stop scrolling after 5 seconds
         document.getElementById('start-button').disabled = false;
     }, 5000);
 
